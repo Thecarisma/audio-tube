e29527196b4be62722a4857a47de5db84453e859
Azeez Adewale <azeezadewale98@gmail.com>
2018-05-23 13:10:08 +0000

    convert css files to dynamic php styles

--- css/elegant-audiotube.php
+++ css/elegant-audiotube.php
@@ -2,5 +2,70 @@
 
 header("Content-type: text/css");
 
+echo '
+.youtube-audio {
+	width:510px ;
+	height:30px;
+	background:#eff0f2 ;
+	display:flex;
+}
+
+.youtube-audio #youtube-audio-play {
+	width:13px ;
+	height:13px;
+	margin:1.5%;
+}
+.youtube-audio #youtube-audio-currenttime {
+	margin-top:1.21%;
+	margin-right:1%;
+}
+.youtube-audio #youtube-audio-duration {
+	margin-top:1.21%;
+	margin-left:1%;
+}
+.youtube-audio #youtube-audio-progressbar {
+	width:50% ;
+	height:20%;
+	margin-top:2.3%;
+	background-color:#dbd6d6;
+}
+.youtube-audio #youtube-audio-progress {
+	height:100%;
+	width:50%;
+	background-color:#e87171;
+    display:inline-block;
+}
+.youtube-audio #youtube-audio-volume {
+	width:13px ;
+	height:13px;
+	margin:1.5%;
+}
+.youtube-audio #youtube-audio-volumebar {
+	width:20% ;
+	height:20%;
+	margin-top:2.3%;
+	border-radius:20%;
+	background-color:#ff0000;
+	margin-right:1%;
+	display:flex;
+}
+.youtube-audio #youtube-audio-volumeprogress {
+	width:100% ;
+	height:100%;
+	border-radius:20%;
+	background-color:#e87171;
+    display:inline-block;
+}
+.youtube-audio #youtube-audio-repeat {
+	width:13px ;
+	height:13px;
+	margin:1.5%;
+}
+
+
+
+'
+
+
 
 ?>
