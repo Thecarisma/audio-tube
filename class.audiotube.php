7138abab11333399f3c855ff8ce0904dad0d188e
Azeez Adewale <azeezadewale98@gmail.com>
2018-05-23 13:15:33 +0000

    fintune AudioTube class to call dynamic styles files

--- class.audiotube.php
+++ class.audiotube.php
@@ -45,7 +45,7 @@ class AudioTube {
 		if ( self::$skin == 0) {
 			wp_register_style('audiotube-style', plugins_url('/css/classic-audiotube.css',__FILE__ ));
 		} else if ( self::$skin == 1 ) {
-			//require_once( AUDIO_TUBE_PLUGIN_DIR . 'css/elegant-audiotube.php' );
+			require_once( AUDIO_TUBE_PLUGIN_DIR . 'css/elegant-audiotube.php' );
 		} else {
 			wp_register_style('audiotube-style', plugins_url('/css/classic-audiotube.css',__FILE__ ));
 		}
