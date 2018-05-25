698251bf51d190e146991797416cc6be53d08e61
Azeez Adewale <azeezadewale98@gmail.com>
2018-05-25 18:50:07 +0000

    create the option and settings page

--- settings.audiotube.php
+++ settings.audiotube.php
@@ -63,66 +63,30 @@ class MySettingsPage
             array( $this, 'sanitize' ) // Sanitize
         );
 		
-        ?>
-		<div class="wrap">
-      <form action="options.php" method="post">
-        <table>
-             
-            <tr>
-                <th>Your name</th>
-                <td><input type="text" placeholder="Your name" name="map_option_1" value="<?php echo esc_attr( get_option('map_option_1') ); ?>" size="50" /></td>
-            </tr>
-            <tr>
-                <th>Your biography</th>
-                <td><textarea placeholder="Your bio" name="map_option_2" rows="5" cols="50"><?php echo esc_attr( get_option('map_option_2') ); ?></textarea></td>
-            </tr>
- 
-            <tr>
-                <th>Your age</th>
-                <td>
- 
-                    <select name="map_option_3">
-                        <option value="">&mdash; select &mdash;</option>
-                        <option value="10-20" <?php echo esc_attr( get_option('map_option_3') ) == '10-20' ? 'selected="selected"' : ''; ?>>10-30</option>
-                        <option value="20-30" <?php echo esc_attr( get_option('map_option_3') ) == '20-30' ? 'selected="selected"' : ''; ?>>20-30</option>
-                        <option value="30-50" <?php echo esc_attr( get_option('map_option_3') ) == '30-50' ? 'selected="selected"' : ''; ?>>30-50</option>
-                    </select>
- 
-                </td>
-            </tr>
- 
-            <tr>
-                <th>Your gender</th>
-                <td>
-                    <label>
-                        <input type="radio" name="map_option_4" value="male" <?php echo esc_attr( get_option('map_option_4') ) == 'male' ? 'checked="checked"' : ''; ?> /> Male <br/>
-                    </label>
-                    <label>
-                        <input type="radio" name="map_option_4" value="female" <?php echo esc_attr( get_option('map_option_4') ) == 'female' ? 'checked="checked"' : ''; ?> /> Female
-                    </label>
-                </td>
-            </tr>
- 
-            <tr>
-                <th>Do you love WordPress?</th>
-                <td>
-                    <label>
-                        <input type="checkbox" name="map_option_5" <?php echo esc_attr( get_option('map_option_5') ) == 'on' ? 'checked="checked"' : ''; ?> />Yes, I love WordPress
-                    </label><br/>
-                    <label>
-                        <input type="checkbox" name="map_option_6" <?php echo esc_attr( get_option('map_option_6') ) == 'on' ? 'checked="checked"' : ''; ?> />No, I love WordPress
-                    </label>
-                </td>
-            </tr>
- 
-            <tr>
-                <td><?php submit_button(); ?></td>
-            </tr>
- 
-        </table>
- 
-      </form>
-    </div> <?php  
+		add_settings_section(
+            'audio_tube_skin_option', // ID
+            'Customize the AudioTube player skin and styles', // Title
+            array( $this, 'empty_info' ), // Callback
+            'audiotube_settings_admin' // Page
+        );
+		
+		//preview
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title 
+            array( $this, 'id_number_callback' ), // Callback
+            'audiotube_settings_admin', // Page
+            'audio_tube_skin_option' // Section           
+        );      
+
+        /**add_settings_field(
+            'title', 
+            'Title', 
+            array( $this, 'title_callback' ), 
+            'audiotube_settings_admin', 
+            'audio_tube_skin_option'
+        ); **/     
     }
 
     /**
