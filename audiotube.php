3df3a2acdf17aa71bf1db6eff4d19eeb76af08f2
Azeez Adewale <azeezadewale98@gmail.com>
2018-05-25 18:08:52 +0000

    create the option and settings page

--- audiotube.php
+++ audiotube.php
@@ -45,7 +45,7 @@ require_once( AUDIO_TUBE_PLUGIN_DIR . 'settings.audiotube.php' );
 if( is_admin() )
     $my_settings_page = new MySettingsPage();
 
-var_export( array( 'MySettingsPage', '$options' ) );
+echo "YEA YEA "+MySettingsPage::$options['title'];
 add_action( 'init', array( 'AudioTube', 'init' ) );
 
 ?>
