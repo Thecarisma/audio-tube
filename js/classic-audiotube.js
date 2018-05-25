ea0b4df7729e6909ceff9c747f4e32afa5b88396
Azeez Adewale <azeezadewale98@gmail.com>
2018-05-25 15:15:02 +0000

    create corresponding script for UI rendering

--- js/classic-audiotube.js
+++ js/classic-audiotube.js
@@ -71,6 +71,7 @@ $(window).bind("load", function() {
 		youtubeAudioRB[i] = rb ;
 		youtubeAudioRepeat[i] = false ;
 	}
+	alert('player style : '+playerStyle);
 	//createLayout();
 //}
 function createLayout() {
